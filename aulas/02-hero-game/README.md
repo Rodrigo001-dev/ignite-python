91a5c115505a96c7ff472aee202f9958ce6c199b
Rodrigo Rael <rodrigorael53@gmail.com>
2024-05-16 09:08:14 -0300

    :sparkles: feat: special attack

---
+++ aulas/02-hero-game/README.md
@@ -0,0 +1,16 @@
+## :page_with_curl: Projeto
+
+![Orientação a objeto com Python]()
+
+> Essa é a segunda aplicação criada na trilha Python da [Rocketseat](https://github.com/Rocketseat), nela o objetivo foi criar um jogo por turnos via terminal para aprender conceitos da orientação a objeto no Python.
+
+## 🚀 Tecnologias/Bibliotecas utilizadas
+
+<a href="https://www.python.org/" target="_blank"> <img src="https://img.shields.io/badge/-Python-BD9100?style=flat-square&logo=Python&logoColor=white" alt="Python"> </a>
+
+## 💻 Autor
+
+Feito com 💜 by Rodrigo Rael
+
+<a href="https://www.linkedin.com/in/rodrigo-rael-a7a4b51a9/" target="_blank"> <img src="https://img.shields.io/badge/-RodrigoRael-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https" alt="Linkedin Rodrigo"> </a>
+<a href="https://img.shields.io/badge/-rodrigorael53@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:rodrigorael53@gmail.com" target="_blank"> <img src="https://img.shields.io/badge/-rodrigorael53@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:rodrigorael53@gmail.com" alt="Gmail Rodrigo"> </a>
--- aulas/02-hero-game/jogo.py
+++ aulas/02-hero-game/jogo.py
@@ -1,3 +1,5 @@
+import random
+
 class Personagem:
   def __init__(self, nome, vida, nivel):
     self.__nome = nome
@@ -15,6 +17,16 @@ class Personagem:
   
   def exibir_detalhes(self):
     return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+  
+  def receber_ataque(self, dano):
+    self.__vida -= dano
+    if self.__vida <= 0:
+      self.__vida = 0
+
+  def atacar(self, alvo):
+    dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
+    alvo.receber_ataque(dano)
+    print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
     
 class Heroi(Personagem):
   def __init__(self, nome, vida, nivel, habilidade):
@@ -27,6 +39,11 @@ class Heroi(Personagem):
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
   
+  def ataque_especial(self, alvo):
+    dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8)
+    alvo.receber_ataque(dano)
+    print(f"{self.get_nome()} usou a habilidade especial: {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
+  
 class Inimigo(Personagem):
   def __init__(self, nome, vida, nivel, tipo):
     super().__init__(nome, vida, nivel)
@@ -38,7 +55,38 @@ class Inimigo(Personagem):
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
   
-heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
+class Jogo:
+  """ Classe orquestradora do jogo """
+
+  def __init__(self) -> None:
+    self.heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
+    self.inimigo = Inimigo(nome="Morcego", vida=80, nivel=5, tipo="Voador")
+  
+  def iniciar_batalha(self):
+    print("Iniciando batalha!")
+
+    while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+      print("\nDetalhes dos Personagens:")
+      print(self.heroi.exibir_detalhes())
+      print(self.inimigo.exibir_detalhes())
+
+      input("Presione Enter para atacar...")
+      escolha = input("Esolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+      if escolha == "1":
+        self.heroi.atacar(self.inimigo)
+      elif escolha == "2":
+        self.heroi.ataque_especial(self.inimigo)
+      else:
+        print("Escolha inválida. Escolha novamente.")
+
+      if self.inimigo.get_vida() > 0:
+        self.inimigo.atacar(self.heroi)
+
+    if self.heroi.get_vida() > 0:
+      print("\nParabéns você venceu a batalha!")
+    else:
+      print("\nVocê foi derrotado!")
+
+jogo = Jogo()
+jogo.iniciar_batalha()
