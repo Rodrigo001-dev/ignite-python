42a9139e6dfc44c2f78cb9cdb65500600a6404a9
Rodrigo Rael <rodrigorael53@gmail.com>
2024-05-18 12:55:01 -0300

    :test_tube: test: list and get tasks

--- aulas/03-to-do-list/tests.py
+++ aulas/03-to-do-list/tests.py
@@ -17,5 +17,21 @@ def test_create_task():
 
   assert "message" in response_json
   assert "id" in response_json
-  
+
   tasks.append(response_json['id'])
+
+def test_get_tasks():
+  response = requests.get(f"{BASE_URL}/tasks")
+  assert response.status_code == 200
+  response_json = response.json()
+
+  assert "tasks" in response_json
+  assert "total_tasks" in response_json
+
+def test_get_task():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert task_id == response_json['id']
