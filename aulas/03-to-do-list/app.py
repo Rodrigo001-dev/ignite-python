0e93e08c874b5f14c3bb7b09903e8cdbfd965292
Rodrigo Rael <rodrigorael53@gmail.com>
2024-05-18 12:36:23 -0300

    :sparkles: feat: CRUD tasks

--- aulas/03-to-do-list/app.py
+++ aulas/03-to-do-list/app.py
@@ -18,5 +18,56 @@ def create_task():
 
   return jsonify({ "message": "Nova tarefa criada com sucesso" })
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list),
+  }
+
+  return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+    
+  return jsonify({ "message": "Não foi possível encontrar a atividade" }), 404
+
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+    
+  if task == None:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+  data = request.get_json()
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+
+  return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+  if not task:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+  
+  tasks.remove(task)
+
+  return jsonify({ "message": "Tarefa deletada com sucesso" })
+
 if __name__ == "__main__":
   app.run(debug=True)
