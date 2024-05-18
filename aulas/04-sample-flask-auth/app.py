aa683d955574771c9c7764c9ae85ca10c68e312e
Rodrigo Rael <rodrigorael53@gmail.com>
2024-05-18 19:46:47 -0300

    :tada: initial commit

---
+++ aulas/04-sample-flask-auth/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from models.user import User
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+db.init_app(app)
+
+if __name__ == '__main__':
+  app.run(debug=True)
