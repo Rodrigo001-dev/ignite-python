4b5561eaa6b0d1edbb7e4298f04522e0f6b1c7dd
Rodrigo Rael <rodrigorael53@gmail.com>
2024-05-20 08:03:26 -0300

    :sparkles: feat: read & update & delete user routes

--- aulas/04-sample-flask-auth/app.py
+++ aulas/04-sample-flask-auth/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -51,6 +51,45 @@ def create_user():
     return jsonify({ "message": "Usuário cadastrado com sucesso" })
 
   return jsonify({ "message": "Dados inválidos" }), 400
+
+@app.route('/user/<int:user_id>', methods=["GET"])
+@login_required
+def read_user(user_id):
+  user = User.query.get(user_id)
+
+  if user:
+    return { "username": user.username }
+  
+  return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route('/user/<int:user_id>', methods=["PUT"])
+@login_required
+def update_user(user_id):
+  data = request.json
+  user = User.query.get(user_id)
+
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+
+    return jsonify({ "message": f"Usuário {user_id} atualizado com sucesso" })
+  
+  return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route('/user/<int:user_id>', methods=["DELETE"])
+@login_required
+def delete_user(user_id):
+  user = User.query.get(user_id)
+
+  if user and user_id == current_user.id:
+    return jsonify({ "message": "Deleçã não permitida" }), 403
+
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({ "message": f"Usuário {user_id} deletado com sucesso" })
+  
+  return jsonify({ "message": "Usuário não encontrado" }), 404
 
 if __name__ == '__main__':
   app.run(debug=True)
