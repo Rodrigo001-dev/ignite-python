673ba1ebc3a8c10344992c31faf741a98d59cf3f
Rodrigo Rael <rodrigorael53@gmail.com>
2024-06-03 13:37:46 -0300

    :test_tube: test: create pix payment

--- aulas/05-sample-flask-socket/payments/pix.py
+++ aulas/05-sample-flask-socket/payments/pix.py
@@ -5,14 +5,14 @@ class Pix:
   def __init__(self) -> None:
     pass
 
-  def create_payment(self):
+  def create_payment(self, base_dir=""):
     bank_payment_id = str(uuid.uuid4())
 
     hash_payment = f'hash_payment_{bank_payment_id}'
 
     img = qrcode.make(hash_payment)
 
-    img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+    img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
     return {
       "bank_payment_id":  bank_payment_id,
