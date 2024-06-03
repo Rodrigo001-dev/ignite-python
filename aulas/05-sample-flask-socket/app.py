a939fd63fda48636b3e11c2f6fa94bdd4b0961fd
Rodrigo Rael <rodrigorael53@gmail.com>
2024-06-03 12:30:13 -0300

    :sparkles: feat: socket setup

--- aulas/05-sample-flask-socket/app.py
+++ aulas/05-sample-flask-socket/app.py
@@ -3,6 +3,7 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -51,6 +53,10 @@ def payment_pix_page(payment_id):
                            qr_code=payment.qr_code,
                            )
 
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected to the server")
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  socketio.run(app, debug=True)
