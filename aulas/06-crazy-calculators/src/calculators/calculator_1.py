2866e98d2132149e2c543ec186b3009d4ab8d25e
Rodrigo Rael <rodrigorael53@gmail.com>
2024-08-07 22:01:02 -0300

    :sparkles: feat: finishin calculator 1

---
+++ aulas/06-crazy-calculators/src/calculators/calculator_1.py
@@ -0,0 +1,42 @@
+from typing import Dict
+from flask import request as FlaskRequest
+
+class Calculator1:
+  def calculate(self, request: FlaskRequest) -> Dict:
+    body = request.json
+    input_data = self.__validate_body(body)
+    splited_number = input_data / 3
+
+    first_process_result = self.__first_process(splited_number)
+    second_process_result = self.__second_process(splited_number)
+    calc_result = first_process_result + second_process_result + splited_number
+    response = self.__format_response(calc_result)
+
+    return response
+
+  def __validate_body(self, body: Dict) -> float:
+    if "number" not in body:
+      raise Exception("body mal formatado!")
+    
+    input_data = body["number"]
+    return float(input_data)
+  
+  def __first_process(self, first_number: float) -> float:
+    first_part = (first_number / 4) + 7
+    second_part = (first_part ** 2) * 0.257
+
+    return second_part
+  
+  def __second_process(self, second_number: float) -> float:
+    first_part = (second_number ** 2.121)
+    second_part = (first_part / 5) + 1
+
+    return second_part
+  
+  def __format_response(self, calc_result: float) -> Dict:
+    return {
+      "data": {
+        "Calculator": 1,
+        "result": round(calc_result, 2)
+      }
+    }
--- aulas/06-crazy-calculators/src/main/routes/calculators.py
+++ aulas/06-crazy-calculators/src/main/routes/calculators.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
-
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator1():
-    print(request)
-    return jsonify({
-        "success": True,
-        }), 200
+    calc = Calculator1()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
